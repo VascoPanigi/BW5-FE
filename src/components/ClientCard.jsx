cc45152fbea6092cd03e3ab9a9f83aa5905e3328
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-12 16:06:04 +0200

    fix fetch homepage

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -1,6 +1,6 @@
-import { Col, Container, Row } from "react-bootstrap"
-import Button from "react-bootstrap/Button"
-import Card from "react-bootstrap/Card"
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 const ClientCard = ({ client }) => {
   return (
     <Container>
@@ -10,16 +10,13 @@ const ClientCard = ({ client }) => {
             <Card.Header>{client.companyName}</Card.Header>
             <Card.Body>
               <Card.Title>Special title treatment</Card.Title>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </Card.Text>
+              <Card.Text>With supporting text below as a natural lead-in to additional content.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
         </Col>
       </Row>
     </Container>
-  )
-}
-export default ClientCard
+  );
+};
+export default ClientCard;
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,41 +1,38 @@
-import ClientCard from "./ClientCard"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { fetchClientListAction, fetchUserInfoAction } from "../redux/actions"
-import { Col, Container, Row } from "react-bootstrap"
+import ClientCard from "./ClientCard";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchClientListAction, fetchUserInfoAction } from "../redux/actions";
+import { Col, Container, Row } from "react-bootstrap";
 
 const HomeComponent = () => {
-  const token = useSelector((state) => state.user.user_bearer.accessToken)
-  const clients = useSelector((state) => state.clients.clients.content)
-  const dispatch = useDispatch()
-  const [fetching, Isfetching] = useState(false)
-  useEffect(() => {
-    if (token) {
-      console.log("Dispatching fetchUserInfoAction with token")
-      dispatch(fetchUserInfoAction(token))
-      dispatch(fetchClientListAction(token))
-      Isfetching(true)
-    }
-  }, [fetching])
+  const token = useSelector((state) => state.user.user_bearer.accessToken);
+  const clients = useSelector((state) => state.clients.clients.content);
+  const dispatch = useDispatch();
+
+  //   const [fetching, Isfetching] = useState(false)
 
   useEffect(() => {
-    console.log("arrivano le aziende")
-  }, [clients])
+    if (token) {
+      console.log("Dispatching fetchUserInfoAction with token");
+      dispatch(fetchUserInfoAction(token));
+      dispatch(fetchClientListAction(token));
+      //   Isfetching(true)
+    }
+  }, [dispatch, token]);
+
   return (
     <Container>
       <Row>
         <h1>Lista Aziende</h1>
         <Col sm={10}>
-          {!fetching ? (
+          {!clients ? (
             <p>non ci sono aziende al momento</p>
           ) : (
-            clients.map((client) => (
-              <ClientCard key={client.id} client={client} />
-            ))
+            clients.map((client) => <ClientCard key={client.id} client={client} />)
           )}
         </Col>
       </Row>
     </Container>
-  )
-}
-export default HomeComponent
+  );
+};
+export default HomeComponent;
